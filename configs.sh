d9c15f83cedf7667b9aff9572a1a3b0d32916352
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-03-19 12:06:23 -0400

    Added global variable files

---
+++ configs.sh
@@ -0,0 +1,18 @@
+# Lucam configs:
+
+export FLOW_PATH=$HOME/trafficFlow
+export DESTINY_HIGH_RESOLUTION_PATH=$HOME/WORKDIR
+export SOURCE_FOLDER_PATH=$HOME/WORKDIR_SRC
+export SOURCE_VIDEO_PATH=$FLOW_PATH/trialVideos
+export SOURCE_IMAGE_PATH=$FLOW_PATH/trialImages
+export MAIN_PATH=$FLOW_PATH/prototipo
+export LOGO_PATH=$MAIN_PATH/watermark
+export INSTALL_PATH=$MAIN_PATH/installationFiles
+# Program paths
+export TRAFFICLIGHT=$FLOW_PATH/surveillancecameratrafficlightcolor
+# Files
+export SERVER_FILE=$MAIN_PATH/web/configs_server.npy
+export INSTALL_FILE=$INSTALL_PATH/datos.npy
+
+sh variables.sh
+
