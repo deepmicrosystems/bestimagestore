c7f8a213c03ec048d1bfd9bd82c72799b8543e45
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-03-19 12:11:47 -0400

    Added simulation mode when not in lucam*

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ parser.add_argument('-s', '--show',   type = bool, default = False,  help = 'Sho
 #parser.add_argument('-s', '--save',   type = bool, default = False,  help = 'Save all low resolution frames')
 args = parser.parse_args()
 
-myRecorder = HighResolutionRecorder(width = 2560,
-                                    height = 1920,
-                                    simulation = True,          # False
-                                    apply_roi = False,
-                                    apply_background = False,
-                                    save_low_resolution = False,
-                                    trafficlight = None)
-
 if __name__ == "__main__":
+    if (os.uname()[1][:5].lower() == 'lucam'):
+        simulation = False 
+    else:
+        simulation = True
+
+    myRecorder = HighResolutionRecorder(width = 2560,
+                                        height = 1920,
+                                        simulation = simulation,          # False
+                                        apply_roi = False,
+                                        apply_background = False,
+                                        save_low_resolution = False,
+                                        trafficlight = None)
+
     while True:
         image = myRecorder.process_new_image()
         ch = cv2.waitKey(1)
